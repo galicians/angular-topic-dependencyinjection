2d2d302fe317ee1b1e3bca6c9e076d36b615b833
galicians <galicians@gmail.com>
2020-05-09 11:31:09 +0100

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION, ReflectiveInjector } from '@angular/core';
+
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'my-app',
@@ -7,8 +9,16 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+  userName: string;
+  userService: UserService;
 
   providerHidden = false;
+
+  constructor() {
+    // create an inject and ask for it to reslove and create a UserService
+    const injector: any = ReflectiveInjector.resolveAndCreate([UserService]);
+    this.userService = injector.get(UserService)
+  }
 
   displayProvider() {
     this.providerHidden = !this.providerHidden;
